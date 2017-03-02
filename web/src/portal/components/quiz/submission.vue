d7561650f64807aca920597935f82238edad8910
Gerald <gera2ld@163.com>
2017-03-02 10:09:46 +0800

    notify if no submission

--- web/src/portal/components/quiz/submission.vue
+++ web/src/portal/components/quiz/submission.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div class="jumbotron" v-if="!solutions.length">
+      <p>No submission yet.</p>
+      <router-link class="btn btn-primary" :to="`/quiz/${$route.params.id}`">Submit a solution</router-link>
+    </div>
     <div class="card mb-3" v-for="solution in solutions">
       <div class="card-header">
         Submitted at {{formatTime(solution.createdAt)}}
@@ -28,7 +32,7 @@ export default {
   created() {
     const {id} = this.$route.params;
     Me.Quiz.model(id).get('solutions').then(data => {
-      this.solutions = data;
+      this.solutions = data.reverse();
     });
   },
   methods: {
